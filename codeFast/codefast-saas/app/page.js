6c05a8d3e373fd3e49f49a78c2c88f1a26f98239
sheikh abdullah <sheikhabdullah.aps@gmail.com>
2025-02-12 01:05:40 +0530

    content removed

--- codeFast/codefast-saas/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main>
-      <button className="btn">Get Started</button>
-    </main>
-  );
-}
--- misc/lab/res/word.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Microsoft Office\Office12\WINWORD.EXE"
